b5eddb60d2027a632cfb1bd146eb12a438c3cd36
PredoJorge <borutinho@hotmail.com>
2021-05-17 13:05:34 -0300

    Projeto inicial de telas não terminado

---
+++ Software Rede Hotelaria/Software Rede Hotelaria/OpADM.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Software_Rede_Hotelaria
+{
+    public partial class OpADM : Form
+    {
+        public OpADM()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpADM_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            Home voltar = new Home();
+            voltar.Show();
+            this.Close();
+        }
+    }
+}
